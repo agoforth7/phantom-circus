4d2533d33e370e026ee661b841c1bd8884b452bc
Amanda Goforth <amandagoforth7@gmail.com>
2016-07-11 14:04:47 -0400

    some updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 var slides = [
-	{ url: '#', title: 'Fire Twirling' },
-	{ url: '#', title: 'Aerialists' },
-	{ url: '#', title: 'Acrobalance' },
-	{ url: '#', title: 'Belly Dancing' },
-	{ url: '#', title: 'Clowns' }
+	{ url: 'assets/images/carousel_poi_01.jpg', title: 'Fire Twirling' },
+	{ url: 'assets/images/carousel_aerialists_01.jpg', title: 'Aerialists' },
+	{ url: 'assets/images/carousel_acrobats_01.jpg', title: 'Acrobalance' },
+	{ url: 'assets/images/carousel_belly_01.jpg', title: 'Belly Dancing' },
+	{ url: 'assets/images/carousel_clown_01.jpg', title: 'Clowns' }
 ];
 
 var sliderCollection = new Backbone.Collection(slides);
@@ -20,27 +20,31 @@ function SliderView (collection) {
 	this.el.on('click', '.arrow-left', function () {
 		_this.hideCurrent();
 
-		if (_this.current < _this.collection.length) {
-			_this.current += 1;
-		} else if (_this.current = _this.collection.length) {
-			_this.current = 0;
+		if (_this.current > 0) {
+			_this.current--;
+		} else {
+			_this.current = _this.collection.length - 1;
 		}
 
 		_this.showNext();
 	});
 
-	this.el.on('click', '.arrow-right', function () {
-		_this.hideCurrent();
-		
-		if (_this.current < _this.collection.length) {
-			_this.current += 1;
-		} else if (_this.current = 0) {
-			_this.current = _this.collection.length;
-		}
+	this.el.on('click', '.arrow-right', this.cycle.bind(this));
 
-		_this.showNext();
-	});
+	setInterval(this.cycle.bind(this), 7000);
 }
+
+SliderView.prototype.cycle = function () {
+	this.hideCurrent();
+
+	if (this.current === this.collection.length - 1) {
+		this.current = 0;
+	} else {
+		this.current++;
+	}
+
+	this.showNext();
+};
 
 SliderView.prototype.hideCurrent = function () {
 	// use this.current to hide the previous SliderImageView
@@ -48,6 +52,7 @@ SliderView.prototype.hideCurrent = function () {
 };
 
 SliderView.prototype.showNext = function () {
+	console.log(this.current);
 	// use this.current to show the next SliderImageView
 	this.childViews[this.current].show();
 };
@@ -65,17 +70,25 @@ SliderView.prototype.render = function () {
 		<img class="arrow-right" src="#">
 	`)
 
+	// console.log(this.el);
+
 	var sliderImages = this.el.find('.slider-images');
 
 	this.collection.each(function (model) {
 		var sliderImage = new SliderImageView(model);
 		// Render each view
 		sliderImage.render();
+		// Hide them by default - should really do this in the CSS (.slider-image { display: none })
+		sliderImage.hide();
 		// Append each view to .slider-images
 		sliderImages.append(sliderImage.el);
 
 		_this.childViews.push(sliderImage);
 	});
+
+	this.childViews[0].show();
+
+	// console.log(sliderImages);
 };
 
 function SliderImageView (model) {
@@ -96,24 +109,40 @@ SliderImageView.prototype.render = function () {
 
 	// create the children elements
 	// set the title
-	this.el.html('<h2>${title}</h2>');
+	this.el.html(`<h2>${title}</h2>`);
 	// add the background-image css property to this.el
-	this.el.css('background-image', 'url("' + url + '")');
+	this.el.css('background-image', 'url(' + url + ')');
 };
 
 SliderImageView.prototype.hide = function () {
+	this.el.hide();
 	// add a class to this.el? remove a class? fade manually?
-	this.el.addClass('visible');
+	// this.el.removeClass('visible');
+	// this.el.addClass('hidden', {
+	// 	visibility: 'hidden',
+	// 	opacity: 0,
+	// 	transition: visibility 0s 2s, opacity 2s linear
+	// });
 };
 
 SliderImageView.prototype.show = function () {
+	this.el.show();
 	// add a class to this.el?
-	this.el.removeClass('visible');
+	// this.el.removeClass('hidden');
+	// this.el.addClass('visible', {
+	// 	visibility: 'visible',
+	// 	opacity: 1,
+	// 	transition: 'opacity 2s linear'
+	// });
 };
 
 var sliderView = new SliderView(sliderCollection);
 
 sliderView.render();
+
+$('#slider-container').append(sliderView.el);
+
+// console.log(sliderView.render());
 
 console.assert(sliderView.current === 0);
 console.assert(sliderView.childViews.length === 5);
@@ -131,12 +160,3 @@ sliderView.el
 	.trigger('click');
 
 // console.assert(sliderView.current === 0);
-
-
-// // start the app!
-
-// var slider = new SliderView(collection);
-
-// slider.render();
-
-// $('#slider-container').append(slider.el);
